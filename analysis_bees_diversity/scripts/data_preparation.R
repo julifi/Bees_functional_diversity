27710e04de7bcb94e8c63a2fbadda40ef4dcd511
alfredburian <flinserl@hotmail.com>
2025-04-08 22:49:53 +0200

    included cleaning up step

--- analysis_bees_diversity/scripts/data_preparation.R
+++ analysis_bees_diversity/scripts/data_preparation.R
@@ -283,6 +283,8 @@ hist(meta$total.exposure, breaks = 40)
 length(which(meta$total.exposure<64))
 length(which(meta$total.exposure<69))
 
+rm(meta.spring, meta.summer, y, y.2, y.3, end.gap, ends, gaps, no.of.gaps, start.gap, starts)
+
 ### 8) start creating species matrix
 
 cm.females<-matrix(nrow = nrow(meta), ncol=length(spec.list))

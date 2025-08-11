bd13746b8d24ca06def39d0bfb85b8b800af07f3
Julia Fischer <fischjul>
2025-08-11 09:49:15 +0200

    Extraction of precipitation data

--- analysis_bees_diversity/scripts/climate_data.R
+++ analysis_bees_diversity/scripts/climate_data.R
@@ -1,10 +1,11 @@
 # BEFORE: run setting.R
 getwd()
 tempdir <- "analysis_bees_diversity/data/temp"
+locdir <- "analysis_bees_diversity/data/temp"
 
 ## Load Libraries ############################################################# 
 library(rdwd); library(RCurl); library(dplyr); library(readxl); library(terra); library(lubridate); 
-library(tidyr); library(dwdradar)
+library(tidyr); library(dwdradar); library(sf)
 #rdwd::updateRdwd()
 
 ## 1. check available .nc-files on the dwd FTP server -------
@@ -34,20 +35,17 @@ plotRadar(rad, main=".nc", proj="nc", extent="nc", layer=1)
 # set temperature reference raster: rad[[1]]
 raster_ref <- rad[[1]]
 
-# 1.2 Precipitation data: ASCII Format
-# historical: 2005-06-01 until today
-# example data: 
-# format: ASCII
+# 1.2 Precipitation data
+# # RADOLAN 'historical' contains missing data (missing layers within one raster stack = missing hours) --> NOT usable! instead use: RADOLAN 'reproduced' 
 
-# different structure than temperature data: choose layer of interest x within readDWD() as selection = x:x; then: call layer with rad[[1]]
-link <- "hourly/radolan/historical/asc/2010/RW-201001.tar" # 25 mB          # data for January 2010
-file <- dataDWD(link, base=gridbase,  dir=tempdir, joinbf=TRUE, read=FALSE) # dbin -> mode=wb
-rad <- readDWD(file, selection=1:400, dividebyten=TRUE)
-plotRadar(rad, main=".asc", layer=1)
-# set precipitation reference raster: rad[[1]]
-# used projection: Polar Steoreographic, Central Meridian 10.0° E , Standard Parallel 60.0° N
-
-raster_ref_prec <- rad[[1]]
+# RADOLAN reproduced: 2005-06-01 until today
+# example data:
+# format: BIN
+link <- "hourly/radolan/reproc/2017_002/bin/2017/RW2017.002_201712.tar.gz"  # 25 MB
+file <- dataDWD(link, base=gridbase, dir=tempdir, joinbf=TRUE, read=FALSE)
+rad <- readDWD(file, selection=1:2)
+plotRadar(rad$dat, main=".binary RW", extent="rw", layer=1)
+raster_ref_prec <- rad$dat[[1]]
 
 
 
@@ -80,6 +78,7 @@ sites_2010 <- as.data.frame(sites_2010)
 sites_s <- vect(sites_2010, geom = c("lon", "lat"), crs="+proj=longlat")
 plot(sites_s)
 
+
 ### 2.1 Temperature data
 # use projection of temperature reference raster (ETRS89-extended / LCC Europe --> EPSG: 3034)
 sites_proj <- project(sites_s, "EPSG:3034")
@@ -89,21 +88,19 @@ plot(sites_proj)
 sites_2010$cell_id_temp <- cellFromXY(raster_ref, crds(sites_proj))  
 
 ### 2.2 Precipitation data
-# information given in metadata on projection of the RADOLAN precipitation raster: 
-proj <- "+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0+a=6370040 +b=6370040 +units=m"
-
-# use projection of precipitation reference raster
-sites_proj_prec <- project(sites_s, "+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0+a=6370040 +b=6370040 +units=m")
+#check projection of precipitation data: 
+crs(raster_ref_prec) # --> no CRS given but information given in metadata on projection of the RADOLAN precipitation raster: "+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0+a=6370040 +b=6370040 +units=m"
+# assign CRS: 
+raster_ref_prec <- projectRasterDWD(raster_ref_prec,
+                                    proj = "radolan")
+# check CRS
+crs(raster_ref_prec) 
+crs(sites_s)
+#--> same CRS as sites_s
 
 # extract grid cell IDs of sites for precipitation data
-sites_2010$cell_id_prec <- cellFromXY(raster_ref_prec, crds(sites_proj)) 
-# problem: precipitation raster have no defined crs/projection
-
-# to do: align crs/projection of sites with the one of the precipitation raster!
-##
-##
-##
-
+sites_2010$cell_id_prec <- cellFromXY(raster_ref_prec, crds(sites_s)) 
+# note: temperature and precipitation raster have different cell IDs for the same Traps as though they have the same projection the raster extent is different 
 
 
 ## 3. choose relevant raster files (hourly resolution) on temp & precipitation from .nc files  -----------------------
@@ -197,6 +194,8 @@ for(i in 1:length(data_samp_clim)){
   data_samp_clim[[i]]$hour <- as.POSIXct(paste0(data_samp_clim[[i]][,1], ' ',data_samp_clim[[i]]$hour), format="%Y-%m-%d %H:%M:%S")
 }
 
+
+
 ## 4. Preparation for data extraction ------------------
 ### 4.1 Temperature data -------------
 # select proper .nc file and within there: proper raster
@@ -244,10 +243,9 @@ for(i in 1:length(data_samp_clim)){
 }
 
 ### 4.2 Precipitation data -------------
-# select proper .tar file and within there: proper raster
-# 1. year + month --> select .tar file 
-# 2. day + start/end (+ daytime) --> select raster within .tar file
-# BUT: different structure than temperature data: choose layer of interest x within readDWD() as selection = x:x; then: call layer with rad[[1]]
+# select proper .tar.gz file and within there: proper raster
+# 1. year + month --> select .tar.gz file 
+# 2. day + start/end (+ daytime) --> select raster within .tar.gz file
 
 # 4.2.1 get name of .tar file of interest --> select proper folder (year) and file (month) --> defines 'link'
 for(i in 1:length(data_samp_clim)){
@@ -255,9 +253,9 @@ for(i in 1:length(data_samp_clim)){
   
   # check if data.frame covers spring or summer season
   if(grepl('dates.spring', colnames(data_samp_clim[[i]]))[1] == TRUE){
-    link_prec <- paste0(format(data_samp_clim[[i]]$dates.spring, "%Y"), "/RW-", format(data_samp_clim[[i]]$dates.spring, "%Y"),format(data_samp_clim[[i]]$dates.spring, "%m"), ".tar")
+    link_prec <- paste0(format(data_samp_clim[[i]]$dates.spring, "%Y"),"/RW2017.002_",format(data_samp_clim[[i]]$dates.spring, "%Y"),format(data_samp_clim[[i]]$dates.spring, "%m"),".tar.gz")
   }else{
-    link_prec <- paste0(format(data_samp_clim[[i]]$dates.summer, "%Y"), "/RW-", format(data_samp_clim[[i]]$dates.summer, "%Y"),format(data_samp_clim[[i]]$dates.summer, "%m"), ".tar")
+    link_prec <- paste0(format(data_samp_clim[[i]]$dates.summer, "%Y"),"/RW2017.002_",format(data_samp_clim[[i]]$dates.summer, "%Y"),format(data_samp_clim[[i]]$dates.summer, "%m"),".tar.gz")
   }
   data_samp_clim[[i]]$link_prec <- link_prec
 }
@@ -270,29 +268,37 @@ for(i in 1:length(data_samp_clim)){
 
 
 # 5. Extraction of data from raster files -------
-# add cell ID to meta-data
-meta$cellID<-sites_2010$cell_id[match(meta$LocTrap, sites_2010$Trap)]
+# 5.1. Create/ load master data.frame -----
 
-## to DO: add cell ID for precipitation data to meta-data: 
-# first: extract cellID of sites for precipitation reference raster (see )
+# add cell ID of temperature and precipitation raster to meta-data
+# cell ID temperature raster:
+meta$cellID_temp<-sites_2010$cell_id_temp[match(meta$LocTrap, sites_2010$Trap)]
+# cell ID precipitation raster:
+meta$cellID_prec<-sites_2010$cell_id_prec[match(meta$LocTrap, sites_2010$Trap)]
 
-#meta$cellID_prec<-sites_2010$cell_id_prec[match(meta$LocTrap, sites_2010$Trap)]
 
-
-# prepare new master data.frame containing all day-hour-site combinations & information on the .tar-file and respective raster layer
-all.dat_prec<-c()
+# prepare master data.frame containing all day-hour-site combinations & information on the .nc-file (temperature raster stack) , .tar-file (precipitation raster stack) and respective raster layer
+# if already done --> load data in line 294
+all.dat<-c()
 for(i in 1:length(data_samp_clim)){
-  x<-data_samp_clim[[i]]; x<-cbind(x[5:8], cellID = rep(meta$cellID[i], nrow(x)), 
+  x<-data_samp_clim[[i]]; x<-cbind(x[5:8], 
+                                   cellID_temp = rep(meta$cellID_temp[i], nrow(x)), 
+                                   cellID_prec = rep(meta$cellID_prec[i], nrow(x)), 
                                    trap = rep(meta$LocTrap[i], nrow(x))) 
-  all.dat_prec<-rbind(all.dat_prec, x)
+  all.dat<-rbind(all.dat, x)
 }
 
-# 5.1 temperature data ----- 
+# save master data.frame "all.dat"
+write.csv(all.dat,"analysis_bees_diversity/data/all.dat.csv", row.names = FALSE)
 
-# now let's run the extraction procedure --> if already done: proceed in line 267
+# load master data.frame "all.dat"
+all.dat <- read.csv("analysis_bees_diversity/data/all.dat.csv")
 
+
+# 5.2 temperature data ----- 
+# now let's run the extraction procedure --> if already done: proceed in line 320
 unique.nc<-unique(all.dat$nc_temp)
-extracted.data<-c()
+extracted.data.temp<-c()
 
 for(i in 1:length(unique.nc)){
   print(i)
@@ -307,54 +313,173 @@ for(i in 1:length(unique.nc)){
     layer <- rad[[unique.layer[k]]]
     
     selection2<- which(all.dat$nc_temp==unique.nc[i] & all.dat$raster_nr==unique.layer[k])
-    unique.cells<-unique(all.dat$cellID[selection2])
+    unique.cells<-unique(all.dat$cellID_temp[selection2])
     extracted<- data.frame(temp = (terra::extract(layer, unique.cells)),
-                           cellID = unique.cells,
-                           layer = rep(unique.layer[k], length(unique.cells)),
-                           nc.ID = rep(unique.nc[i], length(unique.cells)))
+                           cellID_temp = unique.cells,
+                           raster_nr = rep(unique.layer[k], length(unique.cells)),
+                           nc_temp = rep(unique.nc[i], length(unique.cells)))
     colnames(extracted)[1]<-c('temp')
-    extracted.data<-rbind(extracted.data, extracted)
+    extracted.data.temp<-rbind(extracted.data.temp, extracted)
   }
 }
 # save data on extracted temp values
-write.csv(extracted.data,"analysis_bees_diversity/data/extracted.data.temp.csv", row.names = FALSE)
-
-# load data on extracted temp values
-extracted.data <- read.csv("analysis_bees_diversity/data/extracted.data.temp.csv")
+write.csv(extracted.data.temp,"analysis_bees_diversity/data/extracted.data.temp_FULL.csv", row.names = FALSE)
 
 
-# 5.2 precipitation data -----
+# 5.3 precipitation data -----
 unique.tar<-unique(all.dat$link_prec)
-extracted.data.prec<-c()
 
-for(i in 1:length(unique.tar)){
+# for July 2012 (2012/RW-201207.tar) the data in the binary format within 'RADOLAN reproduced 2017_002' (radolan/reproc/2017_002/bin/2012) is incomplete
+# (3 hour missing: 1207061350 (July 6th 13:50); 1207140650 (July 14th 06:50); 1207140750 (July 14th 07:50)) 
+# the missing cdata is included in 'RADOLAN reproduced 2016_003' (radolan/reproc/2016_003/bin/2012)
+# --> in the following loop July 2012 is skipped and the loop has been split into "i in 1:10" (May 2010 to June 2012) and "i in 12:length(unique.tar)" (August 2012 to September 2021) 
+# the data for July 2012 is subsequently extracted from 'RADOLAN reproduced 2016_003'
+
+# 5.3.1 precipitation data: May 2010 - June 2012
+extracted.data.prec_1<-c()
+for(i in 1:10){
   print(i)
-  link <- paste0("/hourly/radolan/historical/asc/2010/", unique.tar[i])
+  link <- paste0("hourly/radolan/reproc/2017_002/bin/", unique.tar[i])
   file <- dataDWD(link, base=gridbase, dir=tempdir, joinbf=TRUE, read=FALSE) 
+  rad <- readDWD(file, dividebyten=FALSE)
   
   selection<-which(all.dat$link_prec==unique.tar[i])
   unique.layer<-unique(all.dat$raster_nr[selection])
   for (k in 1:length(unique.layer)){
     print(k)
-    
-    rad <- readDWD(file, selection=unique.layer[k]:unique.layer[k], dividebyten=TRUE)
-    layer <- rad[[1]]
+    layer <- rad$dat[[unique.layer[k]]]
+    # assign CRS: 
+    layer <- projectRasterDWD(layer,proj = "radolan")
     
     selection2<- which(all.dat$link_prec==unique.tar[i] & all.dat$raster_nr==unique.layer[k])
     unique.cells<-unique(all.dat$cellID_prec[selection2])
     extracted<- data.frame(prec = (terra::extract(layer, unique.cells)),
                            cellID_prec = unique.cells,
-                           layer = rep(unique.layer[k], length(unique.cells)),
-                           tar.ID = rep(unique.tar[i], length(unique.cells)))
+                           raster_nr = rep(unique.layer[k], length(unique.cells)),
+                           link_prec = rep(unique.tar[i], length(unique.cells)))
     colnames(extracted)[1]<-c('prec')
-    extracted.data.prec<-rbind(extracted.data.prec, extracted)
+    extracted.data.prec_1<-rbind(extracted.data.prec_1, extracted)
   }
 }
 
+# save data on extracted prec values
+write.csv(extracted.data.prec_1,"analysis_bees_diversity/data/extracted.data.prec_2010_05_to_2012_06.csv", row.names = FALSE)
+
+
+# 5.3.2 precipitation data: August 2012 - September 2021
+extracted.data.prec_2<-c()
+#for(i in 12:length(unique.tar)){
+#for(i in 28:length(unique.tar)){
+for(i in 43:length(unique.tar)){
+  print(i)
+  link <- paste0("hourly/radolan/reproc/2017_002/bin/", unique.tar[i])
+  file <- dataDWD(link, base=gridbase, dir=tempdir, joinbf=TRUE, read=FALSE) 
+  rad <- readDWD(file, dividebyten=FALSE)
+  
+  selection<-which(all.dat$link_prec==unique.tar[i])
+  unique.layer<-unique(all.dat$raster_nr[selection])
+  for (k in 1:length(unique.layer)){
+    print(k)
+    layer <- rad$dat[[unique.layer[k]]]
+    # assign CRS: 
+    layer <- projectRasterDWD(layer,proj = "radolan")
+    
+    selection2<- which(all.dat$link_prec==unique.tar[i] & all.dat$raster_nr==unique.layer[k])
+    unique.cells<-unique(all.dat$cellID_prec[selection2])
+    extracted<- data.frame(prec = (terra::extract(layer, unique.cells)),
+                           cellID_prec = unique.cells,
+                           raster_nr = rep(unique.layer[k], length(unique.cells)),
+                           link_prec = rep(unique.tar[i], length(unique.cells)))
+    colnames(extracted)[1]<-c('prec')
+    extracted.data.prec_2<-rbind(extracted.data.prec_2, extracted)
+  }
+}
+
+# save data on extracted prec values
+write.csv(extracted.data.prec_2,"analysis_bees_diversity/data/extracted.data.prec_2012_08_to_2021_09.csv", row.names = FALSE)
+
+# 5.3.3 precipitation data: July 2012
+link <- "hourly/radolan/reproc/2016_003/bin/2012/RW2016.003_201207.tar.gz"  # 25 MB
+file <- dataDWD(link, base=gridbase, dir=tempdir, joinbf=TRUE, read=FALSE)
+rad <- readDWD(file, dividebyten=FALSE)
+
+extracted.data.prec_3<-c()
+
+selection<-which(all.dat$link_prec=="2012/RW2017.002_201207.tar.gz")
+unique.layer<-unique(all.dat$raster_nr[selection])
+for (k in 1:length(unique.layer)){
+  print(k)
+  layer <- rad$dat[[unique.layer[k]]]
+  # assign CRS: 
+  layer <- projectRasterDWD(layer,proj = "radolan")
+  
+  selection2<- which(all.dat$link_prec=="2012/RW2017.002_201207.tar.gz" & all.dat$raster_nr==unique.layer[k])
+  unique.cells<-unique(all.dat$cellID_prec[selection2])
+  extracted<- data.frame(prec = (terra::extract(layer, unique.cells)),
+                         cellID_prec = unique.cells,
+                         raster_nr = rep(unique.layer[k], length(unique.cells)),
+                         link_prec = rep("2012/RW2017.002_201207.tar.gz", length(unique.cells)))
+  colnames(extracted)[1]<-c('prec')
+  extracted.data.prec_3<-rbind(extracted.data.prec_3, extracted)
+}
+
+write.csv(extracted.data.prec_3,"analysis_bees_diversity/data/extracted.data.prec_2021_07.csv", row.names = FALSE)
+
+
+# load precipitation data: 
+# May 2010 - June 2012
+extracted.data.prec_1 <- read.csv("analysis_bees_diversity/data/extracted.data.prec_2010_05_to_2012_06.csv")
+
+# August 2012 - September 2021
+extracted.data.prec_2 <- read.csv("analysis_bees_diversity/data/extracted.data.prec_2012_08_to_2021_09.csv")
+
+# July 2012
+extracted.data.prec_3 <- read.csv("analysis_bees_diversity/data/extracted.data.prec_2021_07.csv")
+
+# bind data.frames
+extracted.data.prec <- rbind(extracted.data.prec_1, extracted.data.prec_2, extracted.data.prec_3)
+write.csv(extracted.data.prec,"analysis_bees_diversity/data/extracted.data.prec_FULL.csv", row.names = FALSE)
+
+
+# 5.4 Add temperature & precipitation data to master data.frame -----
+# load temperature data: 
+extracted.data.temp <- read.csv("analysis_bees_diversity/data/extracted.data.temp_FULL.csv")
+# load precipitation data: 
+extracted.data.prec <- read.csv("analysis_bees_diversity/data/extracted.data.prec_FULL.csv")
+
+# check for duplicates:
+extracted.data.temp[duplicated(extracted.data.temp), ] # --> 0 duplicates
+extracted.data.prec[duplicated(extracted.data.prec), ] # --> 0 duplicates
+
+
+# merge all.dat with data on temperature and precipitation
+all.dat.temp <- left_join(all.dat, extracted.data.temp, by = c("raster_nr", "nc_temp", "cellID_temp"), copy=FALSE)
+all.dat.prec <- left_join(all.dat, extracted.data.prec, by = c("raster_nr", "link_prec", "cellID_prec"), copy=FALSE)
+
+all.dat.temp.prec <- left_join(all.dat.temp, extracted.data.prec, by = c("raster_nr", "link_prec", "cellID_prec"), copy=FALSE)
+
+
+temp.NA <- which(is.na(all.dat.temp$temp))
+prec.NA <- which(is.na(all.dat.prec$prec))
+
+# test NA values in extracted.data.prec:
+## e.g.:
+#layer: 270
+#link: 2010/RW2017.002_201005.tar.gz
+link <- "hourly/radolan/reproc/2017_002/bin/2010/RW2017.002_201005.tar.gz"  # 25 MB
+file <- dataDWD(link, base=gridbase, dir=tempdir, joinbf=TRUE, read=FALSE)
+rad <- readDWD(file)
+plotRadar(rad$dat, main=".binary RW", extent="rw", layer=269)
+plotRadar(rad$dat, main=".binary RW", extent="rw", layer=270)
+plotRadar(rad$dat, main=".binary RW", extent="rw", layer=271)
+# --> no data available for respective layer (270)
+
+
+
+
 
 ### next steps:
-#(ii) get the precipitation data: open: l. 91: set projection for precipitation raster and spatial vector of site coordinates -
-#     -> extract cellID_prec --> l. 279: add cellID_prec to meta --> l. 326: extract precipation values from raster
+#(ii) combine extracted.data.temp and extracted.data.prec 
 #(iii) back-transform the data so it is read to use 
 #(iv) training data: run the selection procedure of best model constants (best settings for suitability approach)
 #(v) testing data: compare the two modelling approaches (exposure days vs. suitability scores), accounting 
@@ -393,9 +518,9 @@ for (i in 1:length(input.data)){
     
     # compute the suitability score for temp above and below the temp optimum separately 
     suitability.estimate[below.opt]<- exp(-((placeholder$temp[below.opt]-constants.grid$t.opt[j])/
-                                            (2*constants.grid$sigma[j]))^2)
+                                              (2*constants.grid$sigma[j]))^2)
     suitability.estimate[above.opt]<- 1-((placeholder$temp[above.opt]-constants.grid$t.opt[j])/
-                                         (constants.grid$t.opt[j]- constants.grid$t.max[j]))^2
+                                           (constants.grid$t.opt[j]- constants.grid$t.max[j]))^2
     
     # negative suitability values need to be set to 0
     suitability.estimate[which(suitability.estimate<0)]<-0
@@ -426,7 +551,7 @@ library(vegan)
 seminat<- colnames(site.env.data)[c(5,8:12,16)]
 meta.trapyearseason$hab.div<-apply(site.env.data[ ,c(5,8:12,16)],1, function(x){length(which(x>0))})
 meta.trapyearseason$hab.even<-apply(site.env.data[ ,c(5,8:12,16)],1, function(x){
-diversity(x, index = 'shannon')/log(specnumber(x))})
+  diversity(x, index = 'shannon')/log(specnumber(x))})
 meta.trapyearseason$hab.proportion<-apply(site.env.data[ ,c(5,8:12,16)],1, function(x){sum(x)})
 
 rm(seminat)
@@ -449,7 +574,7 @@ summary(abund.1)
 
 
 rich.1<-lmer(data= meta.trapyearseason, 
-               richness~ exposure_days + season + exposure_days:season + 
+             richness~ exposure_days + season + exposure_days:season + 
                elevation_mean_400m + elevation_range_400m + hab.even + hab.proportion + hab.div+ year +
                (1|location)+(1|year)+(1|site), REML = T)
 summary(rich.1)
@@ -477,7 +602,7 @@ for(i in 1:ncol(output)){print(i)
 
 
 
-  
+
 # next steps:
 # - load in the other predictors and the random effect variables. 
 # - establish the regression model structure and let it run for all data combinations; play around with
